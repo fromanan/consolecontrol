f79b69b1d15d3a0922c0447bef91278bf210ea8e
Tony Froman <33915703+fromanan@users.noreply.github.com>
2023-10-22 11:44:55 -0400

    Minified

--- source/build.ps1
+++
@@ -1,7 +0,0 @@
-# Run msbuild on the solution, in release mode.
-$msbuild ="${env:ProgramFiles}\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe"
-$args = "ConsoleControl.sln /t:Rebuild /p:Configuration=Release"
-
-# Run the command.
-Write-Host "Running: ""$msbuild"" $args"
-& "$msbuild" /p:Configuration=Release /t:Clean,Build ConsoleControl.sln
